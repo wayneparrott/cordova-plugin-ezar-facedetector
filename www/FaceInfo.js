05a033b9c142edd03cb17fa9b42aac76c2c6c714
Wayne Parrott <wayne.n.parrott@gmail.com>
2016-09-14 12:25:35 -0500

    initial impl for eye & mouth positions

--- www/FaceInfo.js
+++ www/FaceInfo.js
@@ -1,8 +1,20 @@
-var FaceInfo = function(left, top, right, bottom) {
+var FaceInfo = function(left, top, right, bottom,
+                    leftEyeX, leftEyeY, rightEyeX, rightEyeY,
+                    mouthX, mouthY) {
     this.left = left;
     this.top = top;
     this.right = right;
     this.bottom = bottom;
+
+    if (leftEyeX && leftEyeX >= 0 && leftEyeY && leftEyeY >= 0) {
+        this.leftEye = {x: leftEyeX, y: leftEyeY};
+    }
+    if (rightEyeX && rightEyeX >= 0 && rightEyeY && rightEyeY >= 0) {
+        this.rightEye = {x: rightEyeX, y: rightEyeY};
+    }
+    if (mouthX && mouthX >= 0 && mouthY && mouthY >= 0) {
+        this.rightEye = {x: mouthX, y: mouthY};
+    }
 };
 
 module.exports = FaceInfo;
